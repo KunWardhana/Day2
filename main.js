8c25ee14e1c900b5380b0efe7f8c5b317e9762ec
KunWardhana <kun_wardhana@yahoo.com>
2021-04-07 11:06:46 +0700

    belajar tentang string concat

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ const myCar = "Cepet"
 // myCar = "Honda Freed"
 console.log(myCar)
 
+// ambil dari html
+
+const user = document.getElementById('user');
+
+// es5
+
+const user_logged_in = "Marcell"
+// user.innerHTML = "Selamat datang, " + user_logged_in;
+
+
+//es6
+user.innerHTML = `Selamat login, ${user_logged_in}`;

9045730decad79cc489434ea22ddece9f05ce404
KunWardhana <kun_wardhana@yahoo.com>
2021-04-07 10:48:56 +0700

    Menambahkan fitur let di javascript

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 
-var myName = "Kun"
-myName = "Chad"
-console.log(myName)
+// var myName = "Kun"
+// myName = "Chad"
+// console.log(myName)
 
-// const gabisa diubah
-const myCar = "Cepet"
-// myCar = "Honda Freed"
-console.log(myCar)
+// // const gabisa diubah
+// const myCar = "Cepet"
+// // myCar = "Honda Freed"
+// console.log(myCar)
 
+// var myName ="Kun"
+// myName = "Richard"
+// console.log(myName)
+
+// // ES6
+// let myNameLet = "Kun"
+// myNameLet = "Calvin"
+// console.log(myNameLet)
+
+const myCarsVar = ["BMW", "Tesla", "Toyota"]
+
+//let tidak global var global
+for (let i = 0; i<myCarsVar.length;i++)
+{
+    console.log("My Cars = " + myCarsVar[i])
+}
+
+console.log(i)
+
+
+let name ="Hendra"
+
+
+// funcion abc(){
+//     console.log(name);
+// }

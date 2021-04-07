aa18fc55b495e080996c387a05acc20a4a5626eb
KunWardhana <kun_wardhana@yahoo.com>
2021-04-07 11:48:22 +0700

    Belajar foreach

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
+// Ambil Element
 
-var myName = "Kun"
-myName = "Chad"
-console.log(myName)
+const skills= document.getElementById('skills')
 
-// const gabisa diubah
-const myCar = "Cepet"
-// myCar = "Honda Freed"
-console.log(myCar)
+//buat variable
+const mySkills = ["Berenang", "Nongkrong", "Git The f-f-f-f FuckOut"]
 
+mySkills.push("Dev ops"); //normal function
+console.log(mySkills);
+
+//foreach
+mySkills.forEach(showskill);
+
+function showskill(mySkill){
+    console.log(mySkill)
+}
+
+//pokoknya for each ngambil semuanya gausa ribet pake variable
+//btw cons kaga bisa diinisialisasi ulang
+//cons bisa di push atau di pop
+
+
+let parent = "<ul>"
+mySkills.forEach((mySkill) => 
+parent  += `<li>${mySkill}</li>`)
+//console.log(mySkill))  // => arrow function
+parent +="</ul>"
+
+skills.innerHTML = parent;

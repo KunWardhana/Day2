b399dae2ab99e75e1bba30ff9f570b92ae48598b
KunWardhana <kun_wardhana@yahoo.com>
2021-04-07 13:45:19 +0700

    Belajar Fetch data dari API

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
+const products = document.getElementById('products')
 
-var myName = "Kun"
-myName = "Chad"
-console.log(myName)
+const urlproduct = ("https://fakestoreapi.com/products")
 
-// const gabisa diubah
-const myCar = "Cepet"
-// myCar = "Honda Freed"
-console.log(myCar)
+function createNode(element) {
+    return document.createElement(element)
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+fetch(urlproduct)
+.then(response => response.json())
+.then((json) => {
+    console.log(json)
+     json.map((product) => {
+     let li = createNode("li")
+     let img = createNode("img")
+     let span = createNode("span")
+
+    img.setAttribute("width", "200")
+    //jangan lupa pake kurung kurawal bukan kurun [] kek spongebob
+     img.src=product.image;
+     span.innerHTML = `${product.title}, ${product.price}`
+
+    append(li, img)
+    append(li, span)
+    append(products, li)
+ })
+
+ 
+})
+.catch((error)=>console.log(error))
+
+
 

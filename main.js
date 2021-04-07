94da722588ef7980f9dca2964ec4e20f84290170
KunWardhana <kun_wardhana@yahoo.com>
2021-04-07 10:25:19 +0700

    menambahkan fitur di javascript yaitu  variable

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('test console')
+
+var myName = "Kun"
+myName = "Chad"
+console.log(myName)
+
+// const gabisa diubah
+const myCar = "Cepet"
+// myCar = "Honda Freed"
+console.log(myCar)
+
